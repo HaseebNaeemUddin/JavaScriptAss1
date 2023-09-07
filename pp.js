2ae436f470445357e1cef026401eb76ce81c4d3c
HaseebNaeemUddin <Sheikh.haseeb001@gmail.com>
2023-09-07 14:22:38 +0500

    work complete

---
+++ pp.js
@@ -0,0 +1,140 @@
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Chapter1>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Q1
+
+alert("Haseeb");
+alert("Sheikh");
+alert("Sheikh.haseeb001@gmail.com");
+alert("0331264970");
+alert("password");
+
+// Q2
+
+alert("You're learning JavaScript!");
+
+// Q3
+
+alert("Pakistan Zindabad");
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Chapter2>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Q1
+var nemNum = 10;
+
+// Q2
+var firstName
+firstName = "Haseeb";
+
+// Q3
+
+var teamName
+teamName = "Pakistan"
+alert(teamName);
+
+// Q4
+
+var bestMan = "Charlie";
+
+bestMan = "John";
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Chapter3>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Q1 and Q2
+
+var caseQty
+caseQty = 144;
+
+// Q3
+var num = 9;
+
+// Q4
+
+var sum
+
+sum = 5 + 3;
+
+// Q5
+
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+// Q6
+
+var num = 7;
+
+num = 10 + num ;
+console.log(num);
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>Chapter4>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+// Q1
+var productCost = 3.45;
+
+// Q2
+
+var nameOfBand;
+
+// Q3
+
+var favouriteNum = 7;
+
+// Q4
+ 
+var haseebNaeemUddin
+
+// Q5
+
+-----legal variables-----------
+myVariable
+_privateVar
+num123
+firstName
+
+// -------illegal vaiable-----------
+123var
+my Variable
+if
+my-var
+
+
+// >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>chapter5>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+// Q1
+
+Modulo %
+
+// Q2
+
+var num1 = 20;
+
+var num2 = 6;
+
+var num = num1 % num2;
+
+console.log(num);
+
+// Q3
+
+var largeNum = 1000 * 2000;
+console.log(largeNum)
+
+// Q4
+
+var variable1 = 10;
+var variable2 = 20;
+
+var result = variable2 - variable1;
+console.log(result);
+
+// Q5
+var myResult = 15 % 7;
+console.log(myResult);
+
+// Q6
+
+var num1 = 10;
+var num2 = 10;
+
+var result = num1 * num2;
+alert(result);
